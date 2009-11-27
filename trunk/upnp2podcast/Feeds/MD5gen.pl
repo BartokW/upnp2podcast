c5a62109a6621ad1df4635db9baba7cb5a2ea055
zadigian <zadigian@762e0102-b932-11de-9846-1b058f2a8eb1>
2009-11-27 23:10:40 +0000

    Updated to auto generate FeedVersions.txt

--- trunk/upnp2podcast/Feeds/MD5gen.pl
+++ trunk/upnp2podcast/Feeds/MD5gen.pl
@@ -1,17 +1,48 @@
 #! /user/bin/perl
 use MD5;
 
+# Get the directory the script is being called from
+$executable = $0;
+$executable =~ m#(\\|\/)(([^\\/]*)\.([a-zA-Z0-9]{2,}))$#;;
+$executablePath = $`;
 
-open(HTMLFILE,"$ARGV[0]");    
+opendir(FEEDS,$executablePath);
+@feedFiles = readdir(FEEDS);
 
-my $fileContents = "";
+@builtIn = ('aRecent','Hulu','Netflix','zExtras','zzPlayONSettings');
 
-print stderr "  + Input File: $inputFile\n";
-
-while(<HTMLFILE>)
+foreach (@feedFiles)
 {
-    $fileContents .= $_;
+    if (/.properties$/)
+    {
+        $fileName = $`;
+        $fileName =~ /_([A-Z]+)$/i;
+        $plugInName = $1;
+        $fileContents = "";
+        open(HTMLFILE,"$executablePath\\$_"); 
+        print stderr "  + Input File: $fileName\n";
+        print stderr "    - PlugIn: $plugInName\n";
+        @isPlugIn = grep(/$plugInName/, @builtIn);
+        $printPlugIn = "";
+        if (!@isPlugIn)
+        {
+            $printPlugIn = $plugInName;
+            print stderr "      + Not Built-in\n";    
+        }
+        while(<HTMLFILE>)
+        {
+            if (/# Version=([0-9]+)/)
+            {
+                $date = $1;
+                print stderr "    - Vesion: $plugInName\n";
+            }
+            $fileContents .= $_;
+        }
+        print stderr "    - MD5: ".MD5->hexhash($fileContents)."\n";
+        $feedFileText .= "$fileName.properties,$date,".MD5->hexhash($fileContents).",http://upnp2podcast.googlecode.com/svn/trunk/upnp2podcast/Feeds/$fileName.properties,$printPlugIn\n"  
+    }
 }
 
-print stderr "    - MD5: (".MD5->hexhash($fileContents).")\n";
+open(FEEDFILE,">FeedVersions.txt");
+print FEEDFILE $feedFileText;
 
