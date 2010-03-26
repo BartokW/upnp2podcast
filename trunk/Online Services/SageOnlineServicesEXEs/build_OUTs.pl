95bb0e2f65a3ed9ba78c7acd94018f8b01d72141
zadigian <zadigian@762e0102-b932-11de-9846-1b058f2a8eb1>
2010-03-26 20:33:07 +0000

    Fixed PerlIO case sensetivity issue

---
+++ trunk/Online Services/SageOnlineServicesEXEs/build_OUTs.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+# Get the directory the script is being called from
+my $executable = $0;
+$executable =~ m#(\\|\/)(([^\\/]*)\.([a-zA-Z0-9]{2,}))$#;
+my $executablePath = $`;
+my $executableEXE  = $3; 
+
+if (@ARGV == 0)
+{
+    print "No Script!\n";
+    exit 0;
+}
+
+my $script = $ARGV[0];
+$script =~ m#(\\|\/)?(([^\\/]*)\.([a-zA-Z0-9]{2,}))$#;
+my $scriptPath = $`;
+my $scriptEXE  = $3; 
+
+print "  + Script : ($script)\n";
+
+my $cwd  = `pwd`;
+my $date = `date`;
+chomp $date;
+
+$cpString = "cp ./$script ./script.pl";
+print "  + Copying Script : ($cpString)\n";
+`$cpString`;
+$perlString = "perl -pi.bak -e \"s/SNIP:BUILT/Built on $date/g\" ./script.pl";
+print "  + editing temp Script : ($perlString)\n";
+`$perlString`;
+$ppString = "pp -N=Comments=\"$scriptEXE.out v1.0 by evilpenguin ($date)\" -c -M PerlIO.pm -o ./$scriptEXE.out ./script.pl";
+print "  + packing : ($ppString)\n";
+`$ppString`;
+$delString = "rm ./script.pl";
+print "  + Deleting : ($delString)\n";
+`$delString`;
